bf340b0d3faff2d1353568384737a7946eb70688
sdnezz <twinknezz@gmail.com>
2024-12-16 23:47:49 +0300

    added name

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//Greeting user
+int main() {
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::cout << name;
+	std::cout << "Hello World from" << name "!" << endl;
+	return 0;
+}
